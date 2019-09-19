a8109998896e21873a900eb211acfb2a987fe0a6
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-09-19 15:46:38 +0300

    fixed-v.2

--- src/Components/Buttons/Buttons.js
+++ src/Components/Buttons/Buttons.js
@@ -1,47 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// // eslint-disable-next-line react/prefer-stateless-function
-// class Buttons extends React.Component {
-
-//   render() {
-//     const {
-//       reverseGoodsList,
-//       sortGoodsList,
-//       resetGoodsList,
-//       sortAlphGoodsList,
-//       handleSelectChange,
-//       value,
-//     } = this.props;
-
-//     return (
-//       <>
-//         <button
-//           type="button"
-//           onClick={reverseGoodsList}
-//         >
-//         Reverse
-//         </button>
-//         <button
-//           type="button"
-//           onClick={sortAlphGoodsList}
-//         >
-//         Sort alphabetically
-//         </button>
-//         <button type="button" onClick={resetGoodsList}>Reset</button>
-//         <button type="button" onClick={sortGoodsList}>Sort</button>
-//         <select
-//           value={value}
-//           onChange={handleSelectChange}
-//         >
-//           {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-//             .map(item => <option value={item} key={item}>{item}</option>)}
-//         </select>
-//       </>
-//     );
-//   }
-// }
-
 const Buttons = props => (
   <>
     <button
